9e15b7e4860a38740dfc964e7ae265680b25590e
Sunny Patel <138172377+sunnypatel-dev@users.noreply.github.com>
2024-11-18 19:47:51 +0000

    project initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <div className="grid place-content-center h-screen border-2 p-10 border-gray-300">
+    <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
