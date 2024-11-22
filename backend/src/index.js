8a2cd432f21803888085e70c57b79df0bea46556
Sunny Patel <sunnypatel.koder@gmail.com>
2024-11-23 00:32:40 +0530

    api route and controller implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,55 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js";
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
+
+import connectToDatabase from "./lib/db.js";
+
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+// import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+// import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
-
-
-app.use('/api/users', userRoute)
-
-
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB  max file size
+    },
+  })
+);
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+// app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+// app.use("/api/stats", statRoutes);
+
+// error handelr
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : err.message,
+  });
+});
+
 app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectToDatabase();
+});
---
+++ backend/src/lib/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export default async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
