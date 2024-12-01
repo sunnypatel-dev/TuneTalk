8c640231a422adef2bdcd812689839764e6d7112
Sunny Patel <sunnypatel.koder@gmail.com>
2024-12-01 16:12:52 +0530

    created left-right sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/albums/:albumId" element={<AlbumPage />} />
       </Route>
     </Routes>
   );
--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -5,6 +5,8 @@ import {
 } from "@/components/ui/resizable";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import LeftSidebar from "./components/LeftSidebar";
+import FriendsActivity from "./components/FriendsActivity";
 
 const MainLayout = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -26,17 +28,16 @@ const MainLayout = () => {
         className="flex-1 flex h-full overflow-hidden p-2"
       >
         {/* <AudioPlayer /> */}
-        left sidebar
         <ResizablePanel
           defaultSize={20}
           minSize={isMobile ? 0 : 10}
           maxSize={30}
         >
-          {/* <LeftSidebar /> */}
+          <LeftSidebar />
         </ResizablePanel>
         <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
         {/*  */}
-        Main content
+
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
           <Outlet />
         </ResizablePanel>
@@ -44,14 +45,13 @@ const MainLayout = () => {
         {!isMobile && (
           <>
             <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-            right sidebar
             <ResizablePanel
               defaultSize={20}
               minSize={0}
               maxSize={25}
               collapsedSize={0}
             >
-              {/* <FriendsActivity /> */}
+              <FriendsActivity />
             </ResizablePanel>
           </>
         )}
